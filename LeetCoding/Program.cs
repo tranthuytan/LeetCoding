2ac4bd01ff0b4e35cf0b0df87e5da4ff3036ae41
tranthuytan <thuytan03vn@gmail.com>
2024-04-03 16:44:36 +0700

    personal last solution for no.5

---
+++ LeetCoding/Program.cs
@@ -0,0 +1,42 @@
+﻿using LeetCoding;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+#region Main() for 5: Longest palindromic substring
+Dictionary<string, int> testcaseForNo5 = new Dictionary<string, int>();
+//palindromic next to each other
+testcaseForNo5.Add("aab", 2);
+testcaseForNo5.Add("aaqwertyuiopsdfghjklzxcvbnm", 2);
+testcaseForNo5.Add("abb", 2);
+testcaseForNo5.Add("qwertyuiopasdfghjklzxcvbnmm", 2);
+//palindromic with a character in center
+testcaseForNo5.Add("aba", 3);
+testcaseForNo5.Add("babad", 3);
+testcaseForNo5.Add("abaqwertyuiopsdfghjklzxcvnm", 3);
+testcaseForNo5.Add("qwertyuiopsdfghjklzxcvnmaba", 3);
+testcaseForNo5.Add("abcba", 5);
+testcaseForNo5.Add("abcbac", 5);
+testcaseForNo5.Add("abcbacdefg", 5);
+testcaseForNo5.Add("banana", 5);
+//no palindromic
+testcaseForNo5.Add("abcdba", 0);
+testcaseForNo5.Add("qwertyuiopasdfghjklzxcvbnm", 0);
+testcaseForNo5.Add("ac", 0);
+
+int noOfTestCase = testcaseForNo5.Count;
+int noOfCurrentTestCase = 1;
+foreach (var test in testcaseForNo5)
+{
+
+    var result = SolutionForNo5.GetLongestPalindromicSubString(test.Key);
+    if (result.Item1 != test.Value)
+    {
+        Console.WriteLine($"Test {noOfCurrentTestCase++}: Failed. Input: {test.Key} - expected: {test.Value} - actual: {result.Item1}");
+    }
+    else
+    {
+        Console.WriteLine($"Test {noOfCurrentTestCase++}: Passed. Input: {test.Key} - expected: {test.Value} - actual: {result.Item1}");
+    }
+}
+#endregion
+//Console.WriteLine("asdfhjkl".Substring(0,2)); // output: as
---
+++ LeetCoding/SolutionForNo5.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCoding
+{
+    public class SolutionForNo5
+    {
+        public static (int, string) GetLongestPalindromicSubString(string s)
+        {
+            if (s.Length==1)
+                return (1, s);
+            (int, string) result = (0, "");
+            bool hasPalindromicSubString = false;
+            bool toReverseCompareString = false;
+            for (int noOfPalindromicCharacter = 1; noOfPalindromicCharacter < s.Length / 2 + 1; noOfPalindromicCharacter++)
+            {
+                //for palindromic next to each other
+                for (int startIndex = 0; startIndex < s.Length; startIndex++)
+                {
+                    try
+                    {
+                        string toBeCompared = s.Substring(startIndex, noOfPalindromicCharacter);
+                        string palindromicNextToEachOtherToCompare = s.Substring(startIndex + noOfPalindromicCharacter, noOfPalindromicCharacter);
+                        if (toReverseCompareString)
+                        {
+                            char[] toBeComparedCharArray = toBeCompared.ToCharArray();
+                            Array.Reverse(toBeComparedCharArray);
+                            toBeCompared = new string(toBeComparedCharArray);
+                        }
+                        if (palindromicNextToEachOtherToCompare == toBeCompared)
+                        {
+                            hasPalindromicSubString = true;
+
+                            if (toBeCompared.Length + palindromicNextToEachOtherToCompare.Length > result.Item1)
+                            {
+                                result.Item2 = toBeCompared + palindromicNextToEachOtherToCompare;
+                                result.Item1 = result.Item2.Length;
+
+                            }
+                        }
+                    }
+                    catch (ArgumentOutOfRangeException e)
+                    {
+                        break;
+                    }
+                }
+                //for palindromic with a separator
+                for (int startIndex = 0; startIndex < s.Length; startIndex++)
+                {
+                    try
+                    {
+                        string toBeCompared = s.Substring(startIndex, noOfPalindromicCharacter);
+                        string separator = s.Substring(startIndex + noOfPalindromicCharacter, 1);
+                        string palindromicWithACharacterInBetweenToCompare = s.Substring(startIndex + noOfPalindromicCharacter+1, noOfPalindromicCharacter);
+
+                        char[] charArray = toBeCompared.ToCharArray();
+                        Array.Reverse(charArray);
+                        var toBeComparedAfterReverse = new string(charArray);
+                        if (palindromicWithACharacterInBetweenToCompare == toBeComparedAfterReverse)
+                        {
+                            hasPalindromicSubString = true;
+                            if (toBeCompared.Length + separator.Length + palindromicWithACharacterInBetweenToCompare.Length > result.Item1)
+                            {
+                                result.Item2 = toBeCompared + separator + palindromicWithACharacterInBetweenToCompare;
+                                result.Item1 = result.Item2.Length;
+                            }
+                        }
+                    }
+                    catch (ArgumentOutOfRangeException e)
+                    {
+                        break;
+                    }
+                }
+                toReverseCompareString = true;
+            }
+            if (!hasPalindromicSubString)
+            {
+                result.Item1 = 0;
+                result.Item2 = "";
+            }
+            
+            return result;
+        }
+    }
+}
